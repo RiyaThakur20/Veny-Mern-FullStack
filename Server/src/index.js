7aa296a891a945fa331b77452ad99d80b544b25c
Riya Singh <rriyathakur.2020@gmail.com>
2026-05-24 03:58:42 -0700

    fix: cors allow all origins

--- Server/src/index.js
+++ Server/src/index.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 
 // ✅ Preflight requests handle karo
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: '50kb' }));
 app.use(express.urlencoded({ extended: true, limit: '50kb' }));
